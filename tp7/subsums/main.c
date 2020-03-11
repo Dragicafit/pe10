c1d26e07395a8400fb7c4b308981337beaa31261
Paul Boursin <paul.boursin@gmail.com>
2020-03-11 10:37:23 +0100

    bruteforce

--- tp7/subsums/main.c
+++ tp7/subsums/main.c
@@ -10,6 +10,7 @@ int b;
 int count;
 
 void rec(int sum, int i) {
+    if(i > nb) count++;
     if(sum + x[i] >= a && sum + x[i] <= b) {
         rec(sum + x[i], i+1);
     }
@@ -25,7 +26,7 @@ int main(int argc, char const *argv[]) {
 
     count = 0;
 
-
+    rec(0, 0);
 
     printf("%i", count);
 
