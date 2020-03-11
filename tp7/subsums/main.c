29efd153733db2963f2b5ba536b23d2689fc172f
berthel <antoine.berthel@hotmail.com>
2020-03-11 10:21:53 +0100

    parser

--- tp7/subsums/main.c
+++ tp7/subsums/main.c
@@ -1,7 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char const *argv[]) {
-    /* code */
+    int nb;
+    int a;
+    int b;
+    fscanf(STDIN_FILENO, "%i %i %i", &nb, &a, &b);
+    int x[nb];
+    for (int i = 0; i < nb; i++) {
+        fscanf(STDIN_FILENO, "%i", &(x[i]));
+    }
     return 0;
 }
